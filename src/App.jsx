081614750171531a93f4441cf40b3c147f8ff8a8
VivekArgSharma <us896592@gmail.com>
2025-08-25 11:42:57 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CodeEditor from "./components/CodeEditor";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#0f0a19] text-gray-500 px-6 py-8">
+      <CodeEditor />
+    </div>
+  );
+}
+
+export default App;
